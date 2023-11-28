5c2aad1aed9dbbb55418312e54bc2255258dd931
Mr.Mao <951416545@qq.com>
2023-11-28 20:53:38 +0800

    fix: graph node mxc change

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,10 +1,9 @@
-import { ApolloClient, createHttpLink, InMemoryCache, useQuery } from '@apollo/client'
-import { GET_LIQUIDITY_RESERVES } from './query'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
 
 // 创建 apollo 客户端
 export const client = new ApolloClient({
   // 与 API 的 HTTP 连接
-  link: createHttpLink({ uri: 'http://mxc-graph-node.mxc.com:8000/subgraphs/name/ianlapham/uniswap-v2-dev' }),
+  link: createHttpLink({ uri: 'https://mxc-graph-node.mxc.com/subgraphs/name/ianlapham/uniswap-v2-dev' }),
   // 缓存实现
   cache: new InMemoryCache()
 })
