5cd3e6e73a05c204dbdb110c71c5065493b4a8f0
Mr.Mao <951416545@qq.com>
2023-11-29 16:07:55 +0800

    feat: add mainnet chain graph config

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,9 +1,15 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
+import { NETWORK_CHAIN_ID } from '../connectors'
+import { ChainId } from '@uniswap/sdk'
 
+const uri =
+  NETWORK_CHAIN_ID === ChainId.MAINNET
+    ? 'https://mxc-graph.mxc.com/subgraphs/name/ianlapham/uniswap-v2-dev'
+    : 'https://mxc-graph-node.mxc.com/subgraphs/name/ianlapham/uniswap-v2-dev'
 // 创建 apollo 客户端
 export const client = new ApolloClient({
   // 与 API 的 HTTP 连接
-  link: createHttpLink({ uri: 'https://mxc-graph-node.mxc.com/subgraphs/name/ianlapham/uniswap-v2-dev' }),
+  link: createHttpLink({ uri }),
   // 缓存实现
   cache: new InMemoryCache()
 })
