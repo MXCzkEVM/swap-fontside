520d27dcca9e4202c7a0ecf8b459db1c7583bdf6
Mr.Mao <951416545@qq.com>
2023-12-06 23:53:58 +0800

    chore: update network change env

--- src/state/lists/hooks.ts
+++ src/state/lists/hooks.ts
@@ -76,6 +76,8 @@ export function listToTokenMap(list: TokenList): TokenAddressMap {
 
 export function useTokenList(url: string | undefined): TokenAddressMap {
   const lists = useSelector<AppState, AppState['lists']['byUrl']>(state => state.lists.byUrl)
+
+  console.log({ lists })
   return useMemo(() => {
     if (!url) return EMPTY_LIST
     const current = lists[url]?.current
